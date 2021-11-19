57cee3725c4dfe6b3bd5e059a217a1719d2497ff
luigicampagnola <58444283+luigicampagnola@users.noreply.github.com>
2021-11-20 00:28:17 +0100

    added cart logo and profile logo

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import Layout from "../Layout/layout";
 import styles from "./header.module.scss";
+import Image from 'next/image'
 
 import SearchIcon from "../Icons/search";
+import CartIcon from "../Icons/cart";
 
 export default function Header() {
   return (
@@ -24,6 +26,19 @@ export default function Header() {
               placeholder="Search for products, brands and more... "
             />
           </form>
+        </div>
+        <div className={styles.rightContent}>
+          <Link href="/cart">
+            <div className={styles.cartContainer}>
+              <CartIcon width={20} height={20} className={styles.cartIcon}/>
+
+            </div>
+          </Link>
+          <Link href="/">
+            <div className={styles.profileContainer}>
+              <Image src="/Perugia.png" height={32} width={32}/>
+            </div>
+          </Link>
         </div>
       </nav>
     </div>
